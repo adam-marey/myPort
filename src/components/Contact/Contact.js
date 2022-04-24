7f387c9481618737717ca1d72aab27c218ea9ed9
Sulaiman Marey <adammarey51@gmail.com>
2022-04-24 13:19:37 -0700

    centered contact me

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,9 +4,10 @@ import { SocialIcon } from 'react-social-icons';
 const Contact = () => {
   return (
     <div className="container mx-auto p-10 lg:w-6/12 lg:pt-28 lg:pt-0">
-      <h1 className="tracking-widest text-2xl text-gray-700 font-black pt-5 pb-4 flex justify-between">
+      <h1 className="tracking-widest text-2xl text-gray-700 font-black pt-5">
         Contact Me
       </h1>
+      <br />
       <div className="">
         <span className="pr-2 cursor-pointer" target="_blank">
           <SocialIcon
