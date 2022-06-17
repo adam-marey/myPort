b5f4d6516a29e45f02c61302f4d2d890eed91fbb
Sulaiman Marey <adammarey51@gmail.com>
2022-06-17 15:35:35 -0700

    fixed bugs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
-
+import { Link } from 'react-router-dom';
 const Contact = () => {
   return (
     <div className="container mx-auto p-10 lg:w-6/12 lg:pt-28 lg:pt-0">
@@ -14,6 +14,14 @@ const Contact = () => {
           help you with it.
         </p>
       </div>
+      {/* <div>
+        <br />
+        <Link to="https://calendly.com/adammarey51/speak-with-sul?month=2022-06">
+          <button className="button-50 py-2 px-5 rounded">
+            Click to book a call with me
+          </button>
+        </Link>
+      </div> */}
       <br />
       <div className="">
         <span className="pr-2 cursor-pointer" target="_blank">
@@ -37,6 +45,15 @@ const Contact = () => {
             target="_blank"
           />
         </span>
+        {/* <span className="pr-2 cursor-pointer">
+          <SocialIcon
+            url={
+              'https://calendly.com/adammarey51/speak-with-sul?month=2022-06'
+            }
+            network="telegram"
+            target="_blank"
+          />
+        </span> */}
       </div>
     </div>
   );
