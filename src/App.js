d29a495f1222a6fb2912e24b1a9b3b39d5d1f4e3
adam <adammarey51@gmail.com>
2023-07-23 22:37:34 -0700

    fixed prod dep

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import Skills from './components/Skills/Skills';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ProjectsContainer from './components/ProjectsContainer/ProjectsContainer';
 import Experience from './components/Experience/Experience';
-import NeighborFave from './components/projectsDetails/NeighborFave';
-import PersonalWeb from './components/projectsDetails/PersonalWeb';
-import EzManage from './components/projectsDetails/EzManage';
-import VirtualKitchen from './components/projectsDetails/VirtualKitchen';
 function App() {
   return (
     <Router>
@@ -16,12 +12,8 @@ function App() {
         <Navbar />
         <Route exact path="/" component={Home} />
         <Route path="/projects" component={ProjectsContainer} />
-        <Route path="/virtual-kitchen" component={VirtualKitchen} />
-        <Route path="/ez-manage" component={EzManage} />
-        <Route path="/personal-web" component={PersonalWeb} />
-        <Route path="/neighbor-fave" component={NeighborFave} />
         <Route path="/skills" component={Skills} />
-        <Route path="/Experience" component={Experience} />
+        <Route path="/experience" component={Experience} />
       </div>
     </Router>
   );
